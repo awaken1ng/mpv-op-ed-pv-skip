105b12a0cd35b437cebbc6ff9529fba47d6efbe4
Awakening <lucidsunlight@yandex.ru>
2020-07-21 20:23:55 +0000

    Update snippet

--- opening-ending-preview-skip.lua
+++ opening-ending-preview-skip.lua
@@ -1,7 +1,8 @@
+local PREV_CHAPTER = nil
 local ENABLED = true
 local KEYBIND = "y"
 local OPENING = {"op", "opening"}
-local ENDING = {"ed", "ending", "preview"}
+local ENDING = {"ed", "ending", "ending credits", "preview", "next episode preview", "endcard"}
 
 local function has_value(tab, val)
   for index, value in ipairs(tab) do
@@ -26,24 +27,39 @@ mp.add_key_binding(KEYBIND, "op-ed-preview-skipper-toggle", function()
 end)
 
 mp.observe_property("chapter", "number", function(_, chapter)
-  if ENABLED == false
-  or chapter == nil
-  or chapter < 0
+  if not ENABLED 
+  or chapter == nil 
+  or chapter < 0 -- can be -1 when going backwards on first chapter
   then return end
 
-  title = mp.get_property("chapter-list/" .. chapter .. "/title"):lower()
+  -- figure out in which direction the chapter was changed
+  if PREV_CHAPTER == nil or chapter > PREV_CHAPTER then
+    forward = true
+    to_chapter = chapter + 1
+  else
+    forward = false
+    to_chapter = chapter - 1
+  end
 
+  title = mp.get_property("chapter-list/" .. chapter .. "/title"):lower()
   if has_value(OPENING, title) then
-    -- to next chapter
-    mp.set_property("chapter", chapter + 1)
+    -- to next or previous chapter
+    mp.set_property("chapter", to_chapter)
   elseif has_value(ENDING, title) then
     chapters = tonumber(mp.get_property("chapter-list/count") - 1)
-    if chapter >= chapters then
+
+    if forward and chapter >= chapters then
       -- on last chapter, to next playlist item
+      -- if we don't do this, we'll still go to the next item, but will get the following error
+      -- The chapter option must be <= {last chapter}: {last chapter + 1}
       mp.command("playlist-next")
     else
-      -- to next chapter
-      mp.set_property("chapter", chapter + 1)
+      -- to next or previous chapter
+      mp.set_property("chapter", to_chapter)
     end
   end
+end)
+
+mp.observe_property("chapter", "number", function(_, chapter)
+  PREV_CHAPTER = chapter
 end)
